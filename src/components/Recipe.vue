f42bc76b07861bca8bdfe038c23ad2d9bfe38995
nmeinhold <43660864+NateMeinhold@users.noreply.github.com>
2019-07-28 19:38:27 -0700

    First commit

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!-- <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      This is where the text goes
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
@@ -26,9 +25,71 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul> -->
+       <h2>Recipe Search</h2>
+     <p>
+       <router-link to="/homophones">What's for dinner?</router-link>
+       </P>
+       
+    <form v-on:submit.prevent="findWords">
+      <p>Find Recipes related to: <input type="text" v-model="ingredient"> <button type="submit">Search</button></p>
+    </form>
+    
+    <ul class="results" v-if="results && results.length > 0">
+      <!-- //need to fix this to be recipes -->
+      <li class="item" v-for="(item, index) of results" :key="index">
+        <p><strong>{{item.word}}</strong></p>
+        <p>{{item.score}}</p>
+      </li>
+    </ul>
+
+    
+    <div class="no-results" v-else-if="results && results.length === 0">
+      <h2>No Recipes Found</h2>
+      <p>Let's look for somthing else</p>
+    </div>
+
+    <ul class="errors" v-if="errors && errors.length > 0">
+      <li v-for="(error, index) of errors" :key="index">
+        {{error.message}}
+      </li>
     </ul>
   </div>
 </template>
+
+// Add API Call
+// <!--<script>
+// // import axios from 'axios'
+
+// // export default {
+// //   name: 'Rhymesaurus',
+// //   data () {
+// //     return {
+// //       results: null,
+// //       errors: [],
+// //       phrase: '',
+// //       rhyme: ''
+// //     }
+// //   }, 
+// //   methods: {
+// //   findWords: function() {
+// //     axios.get('https://api.datamuse.com/words', {
+// //       params: {
+// //         ml: this.phrase,
+// //         rel_rhy: this.rhyme
+// //       }
+// //     })
+// //     .then(response => {
+// //       this.results = response.data;
+// //     })
+// //     .catch(error => {
+// //       this.errors.push(error);
+// //     });
+// //   }
+// // }
+    
+// // }
+// // </script>
 
 <script>
 export default {
