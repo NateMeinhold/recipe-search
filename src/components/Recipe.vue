58626797473ab6579016a793c203af68038e5e37
Nate Meinhold <natesmacbook@Nates-MBP.home>
2024-01-11 17:29:49 -0800

    fixed the proxy bug

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,93 +1,100 @@
 <template>
   <div class="hello">
+    <h2>Recipe Search</h2>
+    <!-- <p> -->
+    <!-- <h4> Instructions:</h4> -->
+    <h4>Pick an ingredient and type it in, let's find something good!</h4>
+    <!-- <router-link to="/reveal">What's for dinner?</router-link> -->
+    <!-- </p> -->
 
-       <h2>Recipe Search</h2>  
-     <!-- <p> -->
-      <!-- <h4> Instructions:</h4> -->
-      <h4>Pick an ingredient and type it in, let's find something good! </h4> 
-       <!-- <router-link to="/reveal">What's for dinner?</router-link> -->
-       <!-- </p> -->
-       
     <form v-on:submit.prevent="findRecipe">
-      <p> <input type="text" v-model="ingredient"> <button type="submit">Search</button></p>
+      <p>
+        <input type="text" v-model="ingredient" />
+        <button type="submit">Search</button>
+      </p>
     </form>
-     <!-- <p>
+    <!-- <p>
       <h4>Tap the button to learn how to make it! </h4> 
        </p> -->
     <section class="results">
-    <ul class="recipeCards" v-if="results && results.length > 0">
-      
-      <li class="card" v-for="(item, index) of results" :key="index">
-       <!--the whole thing: {{item}} -->
-        <button type="button" class="btn btn-secondary" v-on:click="getMeal(item)">{{item.strMeal}}</button>
-        <!-- Image Thumbnail-->
-        <div id="thumbnail">
-        <img :src="item.strMealThumb" :alt="item.strMeal" height="200" width="200">
-        </div>
-      </li>
-    </ul>
-  
+      <ul class="recipeCards" v-if="results && results.length > 0">
+        <li class="card" v-for="(item, index) of results" :key="index">
+          <!--the whole thing: {{item}} -->
+          <button
+            type="button"
+            class="btn btn-secondary"
+            v-on:click="getMeal(item)"
+          >
+            {{ item.strMeal }}
+          </button>
+          <!-- Image Thumbnail-->
+          <div id="thumbnail">
+            <img
+              :src="item.strMealThumb"
+              :alt="item.strMeal"
+              height="200"
+              width="200"
+            />
+          </div>
+        </li>
+      </ul>
 
-    <div class="no-results" v-else-if="results && results.length === 0">
-      <h2>No Recipes Found</h2>
-      <p>Let's look for somthing else</p>
-    <!-- </div> -->
-<!--I think I'm not calling this right...need to do more research-->
-    <ul class="errors" v-if="errors && errors.length > 0">
-      <li v-for="(error, index) of errors" :key="index">
-        {{error.message}}
-        
-      </li>
-    </ul>
-    </div>
+      <div class="no-results" v-else-if="results && results.length === 0">
+        <h2>No Recipes Found</h2>
+        <p>Let's look for somthing else</p>
+        <!-- </div> -->
+        <!--I think I'm not calling this right...need to do more research-->
+        <ul class="errors" v-if="errors && errors.length > 0">
+          <li v-for="(error, index) of errors" :key="index">
+            {{ error.message }}
+          </li>
+        </ul>
+      </div>
     </section>
     <!-- <h4>Tap the button to learn how to make it! </h4>  -->
   </div>
-  
 </template>
 
 <script>
- import axios from 'axios'
+import axios from "axios";
 
+export default {
+  name: "Recipe",
+  data() {
+    return {
+      results: null,
+      errors: [],
+      ingredient: null,
+    };
+  },
+  methods: {
+    getMeal: function(item) {
+      this.$router.push({ name: "reveal", params: { item } });
+    },
 
- export default {
-      name: 'Recipe',
-   data () {
-     return {
-       results: null,
-       errors: [],
-       ingredient: null,
-     }
-   }, 
-   methods: {
-     getMeal: function(item) {
-       this.$router.push({name: "reveal", params: {item}})
-     },
+    findRecipe: function() {
+      //If I can make it work without the proxy cut "https://cors-any..." and the "proxyUrl +"
 
-     
-   findRecipe: function() {
-     //If I can make it work without the proxy cut "https://cors-any..." and the "proxyUrl +"
-
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    // axios.get('https://www.themealdb.com/api/json/v1/1/filter.php', {
-
-     axios.get(proxyUrl + 'https://www.themealdb.com/api/json/v1/1/filter.php', {
-       params: {
-        i: this.ingredient
-       }
-     })
-     .then(response => {
-       this.results = response.data.meals;
-       this.ingredient=""
-     })
-     .catch(error => {
-       this.errors.push(error);
-     });
-   }
- }
-    
-}
- </script>
+      //got rid of this: let proxyUrl = 'https://cors-anywhere.herokuapp.com/' 1/11/24
+      // axios.get('https://www.themealdb.com/api/json/v1/1/filter.php', {
+      //and proxyUrl + in the axios git bracket and it seems to work 1/11/24
+      axios
+        .get("https://www.themealdb.com/api/json/v1/1/filter.php", {
+          params: {
+            i: this.ingredient,
+          },
+        })
+        .then((response) => {
+          this.results = response.data.meals;
+          this.ingredient = "";
+        })
+        .catch((error) => {
+          this.errors.push(error);
+        });
+    },
+  },
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
@@ -107,7 +114,7 @@ a {
 }
 
 .recipeCards {
-   display: grid;
+  display: grid;
   margin: 0 auto;
   grid-gap: 10px;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
@@ -115,16 +122,16 @@ a {
   justify-content: center;
 }
 
-.results{
+.results {
   margin: 0 50px;
-   /* background-image: url('/assets/paperTiny.jpg'); */
+  /* background-image: url('/assets/paperTiny.jpg'); */
 }
 
-.card{
+.card {
   background-image: url("../assets/wallTiny.jpg");
   background-repeat: no-repeat;
   background-attachment: fixed;
-  background-color: #42b983
+  background-color: #42b983;
 }
 
 /* #thumbnail{
@@ -132,6 +139,4 @@ a {
   
 
 } */
-
-
 </style>
